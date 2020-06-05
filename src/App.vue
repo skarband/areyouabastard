b505d82bb86559fca0998d192deffb898b7062b4
Kat <katerina.b-p@hotmail.com>
2020-06-05 17:34:51 +1000

    hello world

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="!quizStart">
+      <h1 style="padding-left:0.5em; padding-right: 0.5em">
+        Are you a bastard?
+      </h1>
+      <img src="@/assets/hat.png" />
+      <button class="button" @click="quizStart = true">
+        Take the quiz to find out
+      </button>
+    </div>
+    <div v-if="quizStart">
+      <Quiz />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quiz from "./components/Quiz.vue";
+document.title = "Are you a bastard?"
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Quiz,
+  },
+  data: () => ({
+    quizStart: false,
+  }),
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Archivo Black", "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000;
+}
+
+body {
+  background-color: #f3f3f3;
+}
+
+img {
+  margin-top: -1em;
+  margin-bottom: 1.5em;
+  max-height: 20em;
+  max-width: 100%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+h1 {
+  letter-spacing: 1px;
+  /* padding-bottom: 0.5em; */
+  padding-top: 1em;
+}
+
+h2 {
+  font-weight: 400;
+  padding-bottom: 0.5em;
+  padding-top: 1.5em;
+}
+
+.button {
+  font-family: "Roboto", sans-serif;
+  text-align: center;
+  text-transform: uppercase;
+  cursor: pointer;
+  font-size: 20px;
+  letter-spacing: 3px;
+  position: relative;
+  border: none;
+  padding: 20px;
+  width: 250px;
+  text-align: center;
+  transition-duration: 0.2s;
+  line-height: 30px;
+  border: 2px solid #000;
+}
+
+.button:hover {
+  background: #000;
+  color: #f3f3f3;
+  /* border: 4px solid #000; */
+}
+
+.button:focus {
+  outline: 0;
+}
+
+a {
+  color: #000;
+  text-decoration: none;
+}
+
+div {
+  width: 100%;
+  max-width: 40em;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,167 @@
+<template>
+  <div id="quiz">
+    <div v-if="!end">
+      <div v-for="(question, index) in questions" v-bind:key="index">
+        <div v-if="index === questionIndex">
+          <h2>{{ question.text }}</h2>
+          <img v-bind:src="require('../assets/' + question.image + '.png')" />
+        </div>
+      </div>
+      <div class="buttonContainer">
+        <button class="button option" style="float: left" v-on:click="yes">
+          Yes
+        </button>
+        <button class="button option" style="float: right" v-on:click="no">
+          No
+        </button>
+      </div>
+    </div>
+    <div v-if="end">
+      <div v-for="(ending, index) in endings" v-bind:key="index">
+        <div v-if="index === questionIndex">
+          <div v-if="!bastard">
+            <h1>Not a bastard.</h1>
+            <img v-bind:src="require('../assets/' + ending.image + '.png')" />
+            <p>{{ ending.text }}</p>
+          </div>
+          <div v-else>
+            <h1>Bastard.</h1>
+            <img v-bind:src="require('../assets/' + ending.image + '.png')" />
+            <p>{{ ending.text }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="buttonContainer">
+        <button style="float: left" class="button restart" v-on:click="restart">
+          Restart
+        </button>
+        <a
+          style="float: right"
+          href="https://secure.actblue.com/donate/ms_blm_homepage_2019"
+          target="_blank"
+          class="button donate"
+          >Donate</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Quiz",
+  props: {},
+  data: () => ({
+    questions: [
+      {
+        text: "Are you a person?",
+        image: "personwaving",
+      },
+      {
+        text: "Do you have a job that pays you?",
+        image: "money",
+      },
+      {
+        text: "Do you typically wear blue or black?",
+        image: "blue",
+      },
+      {
+        text: "Do you have a gun?",
+        image: "gun",
+      },
+      {
+        text:
+          "Do you love firing tear gas at peaceful protestors who fight for an end to police brutality against Black people?",
+        image: "teargas",
+      },
+    ],
+    endings: [
+      {
+        text:
+          "You're a dog. You have no concept of human miseries such as war, capitalism, or Elon Musk. This is the best ending. You win.",
+        image: "matapacos",
+      },
+      {
+        text:
+          "You're a volunteer. You provide a valuable service without even asking for pay. You're pretty great.",
+        image: "volunteer",
+      },
+      {
+        text:
+          "You're a firefighter. You run into burning houses to save kittens. You're the definition of 'protect and serve'. Everyone loves you.",
+        image: "firefighter",
+      },
+      {
+        text:
+          "You're a doctor, or a nurse. You work long hours, and your job is one of the most important ones humanity has. Definitely not a bastard.",
+        image: "doctor",
+      },
+      {
+        text:
+          "You're a professional scuba diver. You probably shoot sharks with your spear gun and pet dolphins, or something.",
+        image: "diver",
+      },
+      {
+        text: "You're a bastard.",
+        image: "bastard",
+      },
+    ],
+    questionIndex: 0,
+    bastard: false,
+    end: false,
+  }),
+  methods: {
+    // Go to next question
+    yes: function() {
+      this.questionIndex++;
+      if (this.questionIndex == 5) {
+        this.bastard = true;
+        this.end = true;
+      }
+    },
+    // Go to previous question
+    no: function() {
+      this.end = true;
+    },
+    restart: function() {
+      this.end = false;
+      this.questionIndex = 0;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.buttonContainer {
+  width: 20em;
+  display: inline-block;
+  overflow: auto;
+  white-space: nowrap;
+  margin: 0px auto;
+}
+
+p {
+  font-family: "Roboto", sans-serif;
+  font-size: 21px;
+}
+
+.option {
+  padding: 20px;
+  width: 100px;
+}
+
+img {
+  border: 2px solid #000;
+}
+
+.restart {
+  padding: 20px;
+  width: 150px;
+}
+
+.donate {
+  padding: 20px;
+  width: 100px;
+}
+</style>
